61e343731e02b48ce75953ee96911403ad82346c
Unknown <mr.hv25@gmail.com>
2018-03-18 21:37:32 +0100

    Header kolejka added

---
+++ inc/kolejka.hh
@@ -0,0 +1,41 @@
+#ifndef KOLEJKA_HH
+#define KOLEJKA_HH
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<stdlib>
+
+using namespace std;
+
+//Typ elementow kolejki priorytetowej
+struct list {
+
+  list *next;
+  int prio;       //priotrytet elementu
+  int data;
+};
+
+
+class queue {
+
+list *head                          //wskaznik na poczatek kolejki
+list *tail                          //wskaznik na koniec kolejki
+
+public:
+  queue();                        //konstruktor kolejki
+  ~queue();                       //destruktor kolejki
+  bool empty();                   // metoda sprawdzajaca czy kolejka jest pusta
+  int front();                    // metoda zwracajaca poczatek kolejki
+  int frontprio();                //metoda zwracajaca priotytet poczatkowego elementu kolejki
+  void push(int prio, int dt)     // metoda zapisujaca element w kolejce
+  void pop();                     // metoda usuwajaca poczatkowy element z kolejki
+};
+
+
+
+
+
+
+
+#endif
